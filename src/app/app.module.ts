c777dfef2bbb46aaa636c9e5ce7aeab8f9bedc0c
KatiaZd <katia.zouad@hotmail.fr>
2023-01-03 13:02:50 +0100

    Création de la page Paiement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PaiementComponent } from './pages/paiement/paiement.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaiementComponent
   ],
   imports: [
     BrowserModule
