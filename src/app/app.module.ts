93c652ba5f3faaba1cbd1e7bac40364d5b490215
Lilianna Guderska <lilianna.guderska@gmail.com>
2023-01-03 15:11:39 +0100

    mis n form component header et footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BarreMenuActionsComponent } from './components/barre-menu-actions/barre-menu-actions.component';
 import { MenuNavigationComponent } from './components/menu-navigation/menu-navigation.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarreMenuActionsComponent,
     MenuNavigationComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
