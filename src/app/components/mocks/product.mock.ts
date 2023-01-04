e09951372ea190782685a6f216f3c3c38faa3ca8
Lilianna Guderska <lilianna.guderska@gmail.com>
2023-01-04 11:58:24 +0100

    dossier mocks - début

---
+++ src/app/components/mocks/product.mock.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    image: string;
+    titre: string;
+}
+export const PRODUCTS: Product []=[
+ {
+    image: "assets/categories/categoriesFemme.png",
+    titre:"FEMME"
+ }   
+]
