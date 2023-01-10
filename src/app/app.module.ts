53d6f3a020e8144d001032f90cae9ad01a8ab9a7
KatiaZd <katia.zouad@hotmail.fr>
2023-01-10 13:01:18 +0100

    Création des components de la page paiement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PaiementComponent } from './pages/paiement/paiement.component';
+import { PanierComponent } from './components/panier/panier.component';
+import { OffrirComponent } from './components/offrir/offrir.component';
+import { LivraisonComponent } from './components/livraison/livraison.component';
+import { MoyenPaiementComponent } from './components/moyen-paiement/moyen-paiement.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PaiementComponent,
+    PanierComponent,
+    OffrirComponent,
+    LivraisonComponent,
+    MoyenPaiementComponent,
   ],
   imports: [
     BrowserModule
