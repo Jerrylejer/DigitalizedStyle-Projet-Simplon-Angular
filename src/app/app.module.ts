f365aafc5adf417a8cf8ccb395b652df38ab31c7
Lilianna Guderska <lilianna.guderska@gmail.com>
2023-01-11 11:07:10 +0100

    supression mock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-
 import { AppComponent } from './app.component';
 
 import { PaiementComponent } from './pages/paiement/paiement.component';
