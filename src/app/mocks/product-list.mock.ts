214fc6d8e07ebbb5b37911c9008306d17b9506b5
Jerrylejer <81814979+Jerrylejer@users.noreply.github.com>
2023-01-12 23:11:18 +0100

    ROUTE DYNAMIQUE ONEPRODUCT OK POUR DOUDOUNES

--- src/app/mocks/product-list.mock.ts
+++ src/app/mocks/product-list.mock.ts
@@ -4,6 +4,8 @@ export interface Item_productlist {
     titre: string;
     icone: string;
     description: string;
+    texte: string;
+    prix: number
  }
  export const ITEMPRODUCTLISTS: Item_productlist[] = [
     {  
@@ -11,41 +13,53 @@ export interface Item_productlist {
        image: "assets/Doudounnes/doudounne2.jpg",
        titre: "Digit-dunne 4.0",
        icone: "assets/wishlistCoeur/coeur1.png",
-       description: "coeur"
+       description: "coeur",
+       texte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+       prix: 2500
     },
     {
        id: 2,
        image: "assets/Doudounnes/doudounneVerte.png",
        titre:"Meta conect-dunne",
        icone: "assets/wishlistCoeur/coeur1.png",
-       description: "coeur"
+       description: "coeur",
+       texte: "Nos super doudounes de la marque DigitaliseStyle viennent de débarquer dans votre boutique online.C’est un nouveau concept innovant.Cette doudoune est fabriqué à partir de bouteilles en plastiques recyclés.La doudoune possède un Qr Code afin d’obtenir son certificat d’authenticité. Le zippée est étanche et monte jusqu’en haut de la capuche. Cette doudoune protège du vent, elle est très légère à porter, confortable puis tient chaud. La matière est très agréable à porter. Cette doudoune sera parfaite pour des sorties en extérieur, des balades en forêt ou encore pour aller au travail ou faire du ski et même faire de la moto. Il vous protégera du froid et du vent glacial durant l'hiver.",
+       prix: 3400
     },
     {
        id: 3,
        image: "assets/Doudounnes/doudounne4.jpg",
        titre:"Space-dunne",
        icone: "assets/wishlistCoeur/coeur1.png",
-       description: "coeur"
+       description: "coeur",
+       texte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+       prix: 1800
     },
     {  
        id: 4,
        image: "assets/Doudounnes/doudounneWhite.jpg",
        titre:"White-numdunne 4.0",
        icone: "assets/wishlistCoeur/coeur1.png",
-       description: "coeur"
+       description: "coeur",
+       texte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+       prix: 2300
     },
     {
        id: 5,
        image: "assets/Doudounnes/doudounne5.jpg",
        titre:"Space connect",
        icone: "assets/wishlistCoeur/coeur1.png",
-       description: "coeur"
+       description: "coeur",
+       texte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+       prix: 3600
     },
     {
        id: 6,
        image: "assets/Doudounnes/doudounne3.png",
        titre:"car-dunne connect",
        icone: "assets/wishlistCoeur/coeur1.png",
-       description: "coeur"
+       description: "coeur",
+       texte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+       prix: 1990
     }
  ]
